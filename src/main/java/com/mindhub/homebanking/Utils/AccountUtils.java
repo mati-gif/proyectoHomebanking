01c8010afb3a1bf01b45d09587618ba2d5e713ba
mati-gif <matiassoria7700@gmail.com>
2024-09-26 11:05:58 -0300

    cambiando la funcinon de accountUtils

--- src/main/java/com/mindhub/homebanking/Utils/AccountUtils.java
+++ src/main/java/com/mindhub/homebanking/Utils/AccountUtils.java
@@ -9,8 +9,8 @@ public class AccountUtils {
 
     public static String generateAccountNumber() {
         Random random = new Random();
-        int min = 10000000; // Valor mínimo para 8 dígitos
-        int max = 99999999; // Valor máximo para 8 dígitos
+        int min = 100; // Valor mínimo para 8 dígitos
+        int max = 999; // Valor máximo para 8 dígitos
         int randomNumber = random.nextInt(max - min + 1) + min;
 
         // Rellenar con ceros a la izquierda si el número tiene menos de 8 dígitos
